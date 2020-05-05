62af1805858f5b6d4dd2bdc6bbcbb98e2dc48c92
hobbyluv <princewillchime43@gmail.com>
2020-05-05 11:12:00 +0100

    leetcode may contest the 5th day problem solving

--- leetcode.js
+++ leetcode.js
@@ -67,7 +67,7 @@ var canConstruct = function(ransomNote, magazine) {
           arr.push(ransomNote[i]);
         }
       }
-      console.log(arr);
+      // console.log(arr);
       if (arr.length === 1) {
         return false;
       } else {
@@ -93,25 +93,50 @@ var canConstruct = function(ransomNote, magazine) {
 
   // return arr;
 };
-console.log(canConstruct("a", "b"));
-console.log(canConstruct("aa", "ab"));
-console.log(canConstruct("aa", "aab"));
-console.log(
-  canConstruct("bg", "efjbdfbdgfjhhaiigfhbaejahgfbbgbjagbddfgdiaigdadhcfcj")
-);
-console.log(canConstruct("fihjjjjei", "hjibagacbhadfaefdjaeaebgi"));
-console.log(canConstruct("fffbfg", "effjfggbffjdgbjjhhdegh"));
-console.log(canConstruct("", ""));
-console.log(canConstruct("", "a"));
+// console.log(canConstruct("a", "b"));
+// console.log(canConstruct("aa", "ab"));
+// console.log(canConstruct("aa", "aab"));
+// console.log(
+//   canConstruct("bg", "efjbdfbdgfjhhaiigfhbaejahgfbbgbjagbddfgdiaigdadhcfcj")
+// );
+// console.log(canConstruct("fihjjjjei", "hjibagacbhadfaefdjaeaebgi"));
+// console.log(canConstruct("fffbfg", "effjfggbffjdgbjjhhdegh"));
+// console.log(canConstruct("", ""));
+// console.log(canConstruct("", "a"));
 
 var findComplement = function(num) {
   var result = Math.pow(2, num.toString(2).length) - 1;
   return ~num & result;
 };
-console.log(findComplement(3));
+// console.log(findComplement(3));
 
-var removeDuplicates = function(nums) {
-  let res = nums.filter((a, b, c) => c.indexOf(a) === b);
-  return res;
+var rotate = function(nums, k) {
+  let resa = nums.reverse();
+  for (let i = 0; i <= resa.length; i++) {
+    if (k) {
+      let res = resa[k - 1];
+      let splix = resa.splice(k, res).reverse();
+      splix.unshift(...resa.reverse());
+      return splix;
+    } else {
+      continue;
+    }
+  }
 };
-console.log(removeDuplicates([1, 1, 2]));
+console.log(rotate([1, 2, 3, 4, 5, 6, 7], 3));
+console.log(rotate([-1, -100, 3, 99], 2));
+
+var firstUniqChar = function(s) {
+  let splitS = s.split("");
+  let objectS = splitS.reduce((ac, cv) => {
+    ac[cv] = (ac[cv] + 1) | 1;
+    return ac;
+  }, {});
+  let keys = Object.keys(objectS).filter(keys => objectS[keys] === 1);
+  let foundValues = splitS.findIndex(
+    firstIndexOfvalueEqualToOne => keys[0] === firstIndexOfvalueEqualToOne
+  );
+  return foundValues;
+};
+console.log(firstUniqChar("leetcode"));
+console.log(firstUniqChar("loveleetcode"));
